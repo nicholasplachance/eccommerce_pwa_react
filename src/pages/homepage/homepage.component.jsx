7ea7b7d91ca16abbf71a8abe3b3904af626c1f9b
Nicholas LaChance <nicholasplachance@gmail.com>
2021-07-04 18:02:48 -0500

    Create repo from project

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+import React from 'react';
+
+const Homepage = () => {
+	return (
+		<div className='homepage'>
+			<Directory />
+		</div>
+	);
+};
+
+export default Homepage;
