15034b5a948b9005da24546b83dab81b2d10c8ef
Nicholas LaChance <nicholasplachance@gmail.com>
2021-07-04 18:45:12 -0500

    Add google font, add images to menu-items, broken homepage into smaller components, finished styling menu-item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,17 @@ import './menu-item.styles.scss';
 
 import React from 'react';
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, id, size }) => {
 	return (
-		<div className='menu-item'>
+		<div className={`${size} menu-item`}>
+			<div
+				className='background-image'
+				style={{
+					backgroundImage: `url(${imageUrl})`
+				}}
+			/>
 			<div className='content'>
-				<h1 className='title'>{title}</h1>
+				<h1 className='title'>{title.toUpperCase()}</h1>
 				<span className='subtitle'>Shop now</span>
 			</div>
 		</div>
