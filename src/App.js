efa4b088f0b9e5ea35b5f2855241958142c2b19c
Nicholas LaChance <nicholasplachance@gmail.com>
2021-07-05 18:10:02 -0500

    Add header navigation and header navigation styling - links to home and shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import { Route, Switch } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import React from 'react';
 import ShopPage from './pages/shoppage/shop.component';
@@ -9,6 +10,7 @@ import ShopPage from './pages/shoppage/shop.component';
 function App() {
 	return (
 		<div className='App'>
+			<Header />
 			<Switch>
 				<Route exact={true} path='/' component={Homepage} />
 				<Route path='/shop' component={ShopPage} />
